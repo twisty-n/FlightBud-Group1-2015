9ba91d0aa57b4d228f79829abf28e4cebc940b3a
Tristan Newmann <tristan_dev@tristans-mbp.newcastle.edu.au>
2015-08-11 14:40:02 +1000

    Added OAuth and JSON parsing libraries to the congif file

---
+++ app/src/main/java/com/flightpub/flightbud/sync/JSONTest.java
@@ -0,0 +1,17 @@
+package com.flightpub.flightbud.sync;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Created by tristan_dev on 11/08/15.
+ */
+public class JSONTest {
+
+    public void test() {
+
+        ObjectMapper x = new ObjectMapper();
+
+    }
+
+}
